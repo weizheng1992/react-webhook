0ac40defc9da591d10ca4504db6e6c08c7dae370
weizheng <weizheng@kaikeba.com>
2021-08-28 21:54:48 +0800

    hook

---
+++ react-bach.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+WORK_PATH = '/usr/projects/vire-react-node'
+cd $WORK_PATH
+echo "先清除老代码"
+git reset --hard origin/main
+git clean -f
+echo "拉去新代码"
+git pull origin main
+echo "开始执行构建"
+docker build -t react-node .
+echo "停止并删除容器"
+docker stop react-back-container
+docker rm react-back-container
+docker container run -p 8099:8099 --name  react-back-container -d react-back
