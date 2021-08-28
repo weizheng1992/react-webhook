7cfdda074bb346efcf61e0bba013c0b2dfe766ef
weizheng <weizheng@kaikeba.com>
2021-08-28 22:15:17 +0800

    wip: dockerfile

--- react-bash.sh
+++ react-bash.sh
@@ -7,8 +7,9 @@ git clean -f
 echo "拉去新代码"
 git pull origin main
 echo "开始执行构建"
-docker build -t react-node .
+docker build -t react-back:1.0 .
 echo "停止并删除容器"
 docker stop react-back-container
 docker rm react-back-container
-docker container run -p 8099:8099 --name  react-back-container -d react-back
+echo "启动新容器"
+docker container run -p 8099:8099 --name  react-back-container -d react-back:1.0
